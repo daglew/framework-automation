124b11c07b7b20e3eda5b01c6a5a383b03a876b5
daglew <daga122611@gmail.com>
2023-02-15 23:22:21 +0100

    preparation framework structure 23

--- utilities/test_status.py
+++ utilities/test_status.py
@@ -8,4 +8,27 @@ class TestStatus(SeleniumDriverHelpers):
 
     def __init__(self, driver):
         super(TestStatus, self).__init__(driver)
-        self.resultList = []
+        self.result_list = []
+
+    def result_set(self, result, result_message):
+        try:
+            if result is not None:
+                if result:
+                    self.result_list.append("PASS")
+                    self.log.info(f"### SUCCESSFUL CHECK ::  {result_message}")
+                else:
+                    self.result_list.append("FAIL")
+                    self.log.info(f"### FAIL CHECK ::  {result_message}")
+            else:
+                self.result_list.append("FAIL")
+                self.log.info(f"### FAIL CHECK ::  {result_message}")
+        except:
+            self.result_list.append("FAIL")
+            self.log.info("### EXCEPTION !!!!!")
+
+    def mark(self, result, result_message):
+        self.result_set(result, result_message)
+
+    def final_mark(self, test_name, result, result_message):
+        print()
+
