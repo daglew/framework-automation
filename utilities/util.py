04bd88b0ab06dbf33561c035aa73afc7078049d0
daglew <daga122611@gmail.com>
2023-02-27 22:54:28 +0100

    preparation framework structure 29

--- utilities/util.py
+++ utilities/util.py
@@ -17,3 +17,25 @@ class Util(object):
         except InterruptedError:
             traceback.print_stack()
 
+    def get_numeric_alpha(self, length, type='letters'):
+        alphanumeric = ''
+        if type =='lower':
+            case = string.ascii_lowercase
+        elif type == 'upper':
+            case = string.ascii_uppercase
+        elif type == 'mix':
+            case = string.ascii_letters + string.digits
+        else:
+            case = string.ascii_letters
+        return alphanumeric.join(random.choice(case) for i in range(length))
+
+    def get_name_unique(self, char_count=10):
+        return self.get_numeric_alpha(length=char_count, type='lower')
+
+    def get_list_unique_names(self, size_list=5, length_item=None):
+        list_name = []
+        for i in range (0, size_list):
+            list_name.append(self.get_name_unique(length_item[i]))
+            return list_name
+
+
