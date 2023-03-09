27e96ec64575faf4275aa18cc88c6fe0416706ef
daglew <daga122611@gmail.com>
2023-03-09 23:25:42 +0100

    additional methods 7

--- tests/courses_test/register_tests_courses.py
+++ tests/courses_test/register_tests_courses.py
@@ -12,11 +12,25 @@ class RegisterTestsCourses(PageBase):
         self.page = self.driver.get("https://courses.letskodeit.com")
 
 
+
 """
 Locators
 """
 sign_in_button_id = "navbar-inverse-collapse"
 password_button_id = "password"
 login_button_id = "login"
+all_courses_click = "ALL COURSES"
+course = "//a[@href='/courses/{0}']"
+enroll_button_link_text = "ENROLL NOW"
+card_number_xpath = "//input[@name='cardnumber']"
+expiry_date_id = "card-expiry"
+security_code_id = "card-cvc"
+enroll_message_error = "//div[@class='card-errors has-error']"
 
+
+    def course_name(self, name):
+        self.
+
+    def course_to_enroll_select(self, full_course_name):
+        print()
 
